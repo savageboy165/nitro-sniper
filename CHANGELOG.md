594a8347f5f7896538973ccfa1abe128439a6ea6
eternal <73202350+slow@users.noreply.github.com>
2021-01-25 14:34:11 +0000

    chore: release

---
+++ CHANGELOG.md
@@ -0,0 +1,153 @@
+## [1.2.6](https://github.com/slow/nitro-sniper/compare/v1.2.5...v1.2.6) (2021-01-25)
+
+
+### Bug Fixes
+
+* adjust timeout to not look sus ([1421e78](https://github.com/slow/nitro-sniper/commit/1421e78b3124ac3a07797bd88fec88ba8d636ee1))
+* webhook erroring if you dont have one set ([f9c64fe](https://github.com/slow/nitro-sniper/commit/f9c64fefbc0e96b8e649f52123ec07b5c379da99))
+* winners doesnt exist when theres only one winner ([a8e3ff6](https://github.com/slow/nitro-sniper/commit/a8e3ff6ed233fa7f41393745ce8dd5790b5f5f7e))
+
+## [1.2.5](https://github.com/slow/nitro-sniper/compare/v1.2.4...v1.2.5) (2021-01-25)
+
+
+### Bug Fixes
+
+* Cannot read property 'id' of undefined ([6e99f15](https://github.com/slow/nitro-sniper/commit/6e99f157f5b21fba7ddd3594df92d26e35c8cc9c))
+
+## [1.2.4](https://github.com/slow/nitro-sniper/compare/v1.2.3...v1.2.4) (2021-01-24)
+
+
+### Bug Fixes
+
+* **heroku env:** brain???????? ([6f3a2a5](https://github.com/slow/nitro-sniper/commit/6f3a2a529d18dec0acdf128e5c8db8896fb2659c))
+
+## [1.2.3](https://github.com/slow/nitro-sniper/compare/v1.2.2...v1.2.3) (2021-01-24)
+
+
+### Bug Fixes
+
+* missing env variables ([15a913b](https://github.com/slow/nitro-sniper/commit/15a913b0db512600829d8f4a268d45578d99bda7))
+
+## [1.2.2](https://github.com/slow/nitro-sniper/compare/v1.2.1...v1.2.2) (2021-01-24)
+
+
+### Reverts
+
+* Revert "fix: attempt to sort environment variables properly on heroku" ([3ebfc93](https://github.com/slow/nitro-sniper/commit/3ebfc9324248a6283d1b88954966f219797831a9))
+
+## [1.2.1](https://github.com/slow/nitro-sniper/compare/v1.2.0...v1.2.1) (2021-01-24)
+
+
+### Bug Fixes
+
+* attempt to sort environment variables properly on heroku ([68e82e4](https://github.com/slow/nitro-sniper/commit/68e82e4870c9401f15d2f53d6324ab8fc4c8c6ce))
+
+# [1.2.0](https://github.com/slow/nitro-sniper/compare/v1.1.8...v1.2.0) (2021-01-24)
+
+
+### Features
+
+* giveaway sniper ([39756e0](https://github.com/slow/nitro-sniper/commit/39756e0488670932a126054f1cd0b0eb01945a22))
+
+## [1.1.8](https://github.com/slow/nitro-sniper/compare/v1.1.7...v1.1.8) (2021-01-24)
+
+
+### Bug Fixes
+
+* performance hit from bufferutil ([7a1ec43](https://github.com/slow/nitro-sniper/commit/7a1ec437c66da4e29f4c7cf176e88b64bb510088))
+
+## [1.1.7](https://github.com/slow/nitro-sniper/compare/v1.1.6...v1.1.7) (2021-01-23)
+
+
+### Bug Fixes
+
+* **embed:** inline location ([c371da5](https://github.com/slow/nitro-sniper/commit/c371da534db6f3e76bf7bd1914df4542ff3df9a8))
+
+## [1.1.6](https://github.com/slow/nitro-sniper/compare/v1.1.5...v1.1.6) (2021-01-23)
+
+
+### Bug Fixes
+
+* **webhook:** author is already a tag ([02125f1](https://github.com/slow/nitro-sniper/commit/02125f1fda9576ef8196daf65c26f49e765bdd9d))
+
+## [1.1.5](https://github.com/slow/nitro-sniper/compare/v1.1.4...v1.1.5) (2021-01-23)
+
+
+### Bug Fixes
+
+* no just no i give up ([f08991e](https://github.com/slow/nitro-sniper/commit/f08991ea959a5dd44aa9250ab387cef255316e1d))
+
+## [1.1.4](https://github.com/slow/nitro-sniper/compare/v1.1.3...v1.1.4) (2021-01-22)
+
+
+### Bug Fixes
+
+* idk anymore man ([81a1e07](https://github.com/slow/nitro-sniper/commit/81a1e07ceef4527be6bc951e74bacf1352da6bca))
+
+## [1.1.3](https://github.com/slow/nitro-sniper/compare/v1.1.2...v1.1.3) (2021-01-22)
+
+
+### Bug Fixes
+
+* please end my suffering ([652b569](https://github.com/slow/nitro-sniper/commit/652b569791963539ab0c05471cdf83dde6c1be42))
+
+## [1.1.2](https://github.com/slow/nitro-sniper/compare/v1.1.1...v1.1.2) (2021-01-22)
+
+
+### Bug Fixes
+
+* i will keep trying :angry: ([aba8afe](https://github.com/slow/nitro-sniper/commit/aba8afec6f83c65e2de5622edc747f078e04bb2a))
+
+## [1.1.1](https://github.com/slow/nitro-sniper/compare/v1.1.0...v1.1.1) (2021-01-22)
+
+
+### Bug Fixes
+
+* :angry: ([5d5783f](https://github.com/slow/nitro-sniper/commit/5d5783f0579483c26a81f230a7cd2a74bba308c7))
+
+# [1.1.0](https://github.com/slow/nitro-sniper/compare/v1.0.4...v1.1.0) (2021-01-22)
+
+
+### Bug Fixes
+
+* semantic release erroring ([b44c065](https://github.com/slow/nitro-sniper/commit/b44c0653b797c68272b6c6f49ca8ee04c39b06c9))
+
+
+### Features
+
+* docker image ([c871aa7](https://github.com/slow/nitro-sniper/commit/c871aa7eb7f22ea4a1bf2c7ab5bfb42dc4db9811))
+
+## [1.0.4](https://github.com/slow/nitro-sniper/compare/v1.0.3...v1.0.4) (2021-01-22)
+
+
+### Bug Fixes
+
+* semantic versioning ([eb28393](https://github.com/slow/nitro-sniper/commit/eb283935a6fdd194f58a6b67147d09d4ee0990dd))
+
+## [1.0.3](https://github.com/slow/nitro-sniper/compare/v1.0.2...v1.0.3) (2021-01-22)
+
+
+### Bug Fixes
+
+* impact less performance ([37d14ec](https://github.com/slow/nitro-sniper/commit/37d14ec6808763affb7482cd7096b1cf5f5fea77))
+
+## [1.0.2](https://github.com/slow/nitro-sniper/compare/v1.0.1...v1.0.2) (2021-01-22)
+
+
+### Bug Fixes
+
+* **variable:** use variable instead of value ([9a7da63](https://github.com/slow/nitro-sniper/commit/9a7da63463fa146ccdbbeadb5587bb845bed7679))
+
+## [1.0.1](https://github.com/slow/nitro-sniper/compare/v1.0.0...v1.0.1) (2021-01-22)
+
+
+### Bug Fixes
+
+* deploy error ([28ff239](https://github.com/slow/nitro-sniper/commit/28ff2391a0ba9a5b0e48d8809c414eb251f615f3))
+
+# 1.0.0 (2021-01-22)
+
+
+### Bug Fixes
+
+* test ci ([e9ea0d9](https://github.com/slow/nitro-sniper/commit/e9ea0d981b28a6defe5897666d62e43397e88750))
---
+++ index.js
@@ -0,0 +1,252 @@
+const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36';
+const regex = new RegExp(/(discord.gift|discord.com|discordapp.com\/gifts)\/\w{16,25}/gim);
+const consumedCodes = [];
+
+require('dotenv').config({ path: 'dotenv' });
+
+const phin = require('phin').unpromisified;
+const { Client, WebhookClient, RichEmbed } = require('discord.js');
+const chalk = require('chalk');
+
+const tokens = process.env.guildTokens.split(',');
+const mainToken = process.env.mainToken;
+const webhookUrl = process.env.webhookUrl;
+const snipeGiveaways = Boolean(process.env.snipeGiveaways) || true;
+const dmHosterOnGiveawayWin = Boolean(process.env.dmHosterOnGiveawayWin) || true;
+const snipeGiveawaysOnlyNitro = Boolean(process.env.snipeGiveawaysOnlyNitro) || false;
+const giveawayWinDM = process.env.giveawayWinDM || 'Hey, i won the giveaway. Could i redeem my prize?';
+
+let paymentMethod = null;
+
+phin({
+   url: 'https://discord.com/api/v6/users/@me/billing/payment-sources',
+   method: 'GET',
+   parse: 'json',
+   headers: {
+      'Authorization': mainToken,
+      'User-Agent': userAgent
+   }
+}, (err, res) => {
+   if (err) console.log(err);
+   if (res.body['message'] == '401: Unauthorized') {
+      console.log(chalk.red('[Sniper] Invalid main token, terminating process.'));
+      process.exit(-1)
+   } else if (res.body.length && res.body.length === 0) {
+      console.log(chalk.red('[Sniper] Main token does not have a billing source, some codes will not be sniped.'));
+   } else if (res.body[0]) {
+      paymentMethod = res.body[0].id;
+   } else {
+      console.log(chalk.red(`[Sniper] Unable to get billing source: ${res.body}`))
+   }
+})
+
+if (webhookUrl != null) {
+   const webhooktoken = /[^/]*$/.exec(webhookUrl)[0];
+   const webhookid = webhookUrl.replace(/^.*\/(?=[^\/]*\/[^\/]*$)|\/[^\/]*$/g, '');
+   const webhookclient = new WebhookClient(webhookid, webhooktoken);
+   function fireHook(opts) {
+      let embed = new RichEmbed();
+      if (opts.title) embed.setTitle(opts.title);
+      if (opts.color) embed.setColor(opts.color);
+      for (const field of opts.fields) {
+         embed.addField(field.key, field.value, field.inline);
+      }
+      webhookclient.send('', { embeds: [embed] }).catch(() => {
+         if (webhookUrl) {
+            console.log(chalk.red("[Sniper] Couldn't reach webhook. Your webhook URL is invalid."));
+         }
+      });
+   };
+};
+
+for (const token of tokens) {
+   const client = new Client({
+      messageCacheLifetime: 1,
+      messageCacheMaxSize: 1,
+      messageSweepInterval: 1,
+      messageEditHistoryMaxSize: 1,
+      restTimeOffset: 0,
+      disabledEvents: [
+         'TYPING_START',
+         'VOICE_SERVER_UPDATE',
+         'RELATIONSHIP_ADD',
+         'RELATIONSHIP_REMOVE',
+         'GUILD_ROLE_DELETE',
+         'GUILD_ROLE_UPDATE',
+         'GUILD_BAN_ADD',
+         'GUILD_BAN_REMOVE',
+         'CHANNEL_UPDATE',
+         'CHANNEL_PINS_UPDATE',
+         'MESSAGE_DELETE',
+         'MESSAGE_UPDATE',
+         'MESSAGE_DELETE_BULK',
+         'MESSAGE_REACTION_ADD',
+         'MESSAGE_REACTION_REMOVE',
+         'GUILD_MEMBER_UPDATE',
+         'GUILD_MEMBERS_CHUNK',
+         'GUILD_ROLE_CREATE',
+         'MESSAGE_REACTION_REMOVE_ALL',
+         'USER_UPDATE', 'USER_NOTE_UPDATE',
+         'USER_SETTINGS_UPDATE',
+         'PRESENCE_UPDATE',
+         'VOICE_STATE_UPDATE',
+         'GUILD_UPDATE',
+         'GUILD_MEMBER_ADD',
+         'GUILD_MEMBER_REMOVE'
+      ]
+   });
+
+   client.on('message', async (msg) => {
+      let codes = msg.content.match(regex);
+      let author = msg.author.tag;
+      let from = msg.guild ? `${msg.guild.name} (Server)` : 'DMs';
+
+      if (codes && codes.length) {
+         for (let code of codes) {
+            code = code.replace(/(discord\.gift\/|discord\.com\/gifts\/|discordapp\.com\/gifts\/)/gim, '');
+
+            let start = new Date();
+
+            if (consumedCodes.indexOf(code) > -1) {
+               console.log(chalk.gray(`[Sniper] Avoiding Duplicate - Code: ${chalk.bold(code)} - ${from} (${author})`));
+               continue;
+            }
+
+            let payload = `{"channel_id":${msg.channel.id},"payment_source_id":${paymentMethod}}`;
+            phin({
+               url: `https://discord.com/api/v6/entitlements/gift-codes/${code}/redeem`,
+               method: 'POST',
+               parse: 'json',
+               headers: {
+                  'Authorization': mainToken,
+                  'User-Agent': userAgent,
+                  'Content-Length': payload.length
+               },
+               data: payload
+            }, (err, res) => {
+               let end = `${new Date() - start}ms`;
+               let type = res.body.subscription_plan?.name;
+               if (err) {
+                  return console.log(chalk.red(`[Sniper] Error - Code: ${chalk.bold(code)} - ${err} - ${from} (${author}) - ${end}`));
+               } else if (res.body.message === '401: Unauthorized') {
+                  return console.log(chalk.red(`[Sniper] Error - Code: ${chalk.bold(code)} - Your main token is invalid.`));
+               } else if (res.body.message == 'This gift has been redeemed already.') {
+                  console.log(chalk.red(`[Sniper] Already Redeemed - Code: ${chalk.bold(code)} - ${from} (${author}) - ${end}`));
+               } else if ('subscription_plan' in res.body) {
+                  console.log(chalk.green(`[Sniper] Success - Code: ${chalk.bold(code)} - ${type} - ${from} (${author}) - ${end}`));
+                  fireHook({
+                     title: 'Nitro Sniped',
+                     fields: [
+                        {
+                           key: 'Time Taken',
+                           value: end,
+                           inline: true
+                        },
+                        {
+                           key: 'Type',
+                           value: type,
+                           inline: true
+                        },
+                        {
+                           key: 'Code',
+                           value: code,
+                           inline: true
+                        },
+                        {
+                           key: 'Account',
+                           value: client.user.tag,
+                           inline: true
+                        },
+                        {
+                           key: 'Author',
+                           value: author,
+                           inline: true
+                        },
+                        {
+                           key: 'Location',
+                           value: from,
+                           inline: true
+                        },
+                     ],
+                     color: '#41FC9F'
+                  });
+               } else if (res.body.message == 'Unknown Gift Code') {
+                  console.log(chalk.red(`[Sniper] Invalid Code - Code: ${chalk.bold(code)} - ${from} (${author}) - ${end}`));
+               }
+               consumedCodes.push(code);
+            });
+         }
+      };
+      if (snipeGiveaways) {
+         // GiveawayBot#2381 (294882584201003009)
+         if (msg.author.id == '294882584201003009' && msg.content.includes('GIVEAWAY')) {
+            let timeout = randomInt(10e3, 30e3);
+            let prize = msg.embeds[0].author.name;
+            if (snipeGiveawaysOnlyNitro && !prize.toLowerCase().includes('nitro')) return;
+            console.log(chalk.gray(`[Sniper] Giveaway detected in ${from}, reacting in ${((timeout % 60000) / 1000).toFixed(0)} seconds.`))
+
+            setTimeout(async () => {
+               let react = await msg.react('🎉').catch(() => null);
+               if (react) console.log(chalk.green(`[Sniper] Reacted to giveaway in ${from} (Prize: ${prize})`));
+            }, timeout)
+         }
+         if (msg.author.id == '294882584201003009' && msg.content.includes('Congratulations') && msg.mentions.users.first()?.id == client.user.id) {
+            let messageLink = msg.content.replace(/\r/g, "").split(/\n/)[1].replace(/(<|>)/g, '');
+            let giveawayMessageId = messageLink.split('/')[6];
+
+            let message = await msg.channel.fetchMessage(giveawayMessageId).catch(() => null);
+            if (message) {
+               let prize = message.embeds[0].author.name;
+               let description = message.embeds[0].description.replace(/\r/g, "").split(/\n/);
+
+               let hosterId = description[1].split('<@')[1].replace('>', '');
+               await client.fetchUser(hosterId);
+               let hoster = client.users.get(hosterId);
+
+               console.log(chalk.green(`[Sniper] Giveaway Won in ${from} (Prize: ${prize})`));
+               fireHook({
+                  title: 'Giveaway Won',
+                  fields: [
+                     {
+                        key: 'Prize',
+                        value: prize,
+                        inline: true
+                     },
+                     {
+                        key: 'Location',
+                        value: from,
+                        inline: true
+                     },
+                     {
+                        key: 'Hoster',
+                        value: hoster.tag,
+                        inline: true
+                     }
+                  ],
+                  color: '#41FC9F'
+               })
+
+               if (dmHosterOnGiveawayWin) {
+                  let sentMsg = await hoster.send(giveawayWinDM).catch(() => null);
+                  if (sentMsg) console.log(chalk.gray(`[Sniper] DMed giveaway hoster (${hoster.tag}) - ${giveawayWinDM}`))
+                  else console.log(chalk.red(`[Sniper] Failed to DM giveaway host (${hoster.tag}) - This might require you to manually log in.`));
+               }
+            }
+         }
+      }
+   });
+
+   client.on('ready', () => {
+      console.log(chalk.green(`[Sniper] Logged in as ${client.user.tag}.`));
+   });
+
+   setTimeout(() => {
+      client.login(token).catch(err => {
+         console.log(chalk.red(`[Sniper] Skipping alt token - ${err} - ${token}`))
+      });
+   }, randomInt(1e3, 3e3));
+
+   function randomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+   }
+}
